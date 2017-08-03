8ab0c04c55bc9695d10796bbc076a415931d115f
Shivam Rastogi <shivam@Shivams-MacBook-Air.local>
2017-08-03 10:19:07 +0530

    pp01: Completed basic structure, added styles for nav

---
+++ 1-pp01-personal-portfolio/scrolling-nav.js
@@ -0,0 +1,26 @@
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $(document).on('click', 'a.page-scroll', function(event) {
+        var $anchor = $(this);
+
+      // $('a').each(function () {
+      //      $(this).removeClass('active');
+      //  })
+      //  $(this).addClass('active');
+
+       $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+
+        event.preventDefault();
+    });
+});
