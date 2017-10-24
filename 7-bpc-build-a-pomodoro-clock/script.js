a19d19658f8d3d16383396b3cee5def355199012
Shivam Rastogi <shivam.ras304@gmail.com>
2017-10-24 21:40:32 +0530

    bpc: Added major styling

--- 7-bpc-build-a-pomodoro-clock/script.js
+++ 7-bpc-build-a-pomodoro-clock/script.js
@@ -1,5 +1,3 @@
-
-
 window.onload = function() {
 
   var breakLengthDisplay = document.querySelector("#length-break");
@@ -8,7 +6,7 @@ window.onload = function() {
   var clockHeading = document.querySelector("#clock-heading");
   var clockTime = document.querySelector("#clock-time");
 
-  var playPauseButton = document.querySelector("#play-pause");
+  var clock = document.querySelector("#clock");
   var resetButton = document.querySelector("#reset");
 
   var isClockRunning = false;
@@ -18,40 +16,41 @@ window.onload = function() {
   //These values should be the same as in the html code
   //The following two variables will only be altered manually
   var breakLength = 5;
-  var sessionLength = 10;
+  var sessionLength = 25;
 
   //The following two variable will be altered in code
-  var breakLengthSecs = breakLength*60;
-  var sessionLengthSecs = sessionLength*60;
+  var breakLengthSecs = breakLength * 60;
+  var sessionLengthSecs = sessionLength * 60;
 
   var sound = new Audio("https://www.freespecialeffects.co.uk/soundfx/sirens/big_bell.wav");
 
   window.setBreakLength = function(target) {
-    if(isClockRunning) return;
+    if (isClockRunning) return;
 
-    if(target.id === "up-break") {
+    if (target.id === "up-break") {
       breakLength++;
-    } else if(breakLength > 1){
+    } else if (breakLength > 1) {
       breakLength--;
     }
 
     breakLengthDisplay.innerText = breakLength;
-    breakLengthSecs = breakLength*60;
+    breakLengthSecs = breakLength * 60;
   }
 
   window.setSessionLength = function(target) {
-    if(isClockRunning) return;
+    if (isClockRunning) return;
 
-    if(target.id === "up-session") {
+    if (target.id === "up-session") {
       sessionLength++;
-    } else if(sessionLength > 1){
+    } else if (sessionLength > 1) {
       sessionLength--;
     }
     sessionLengthDisplay.innerText = sessionLength;
-    sessionLengthSecs = sessionLength*60;
+    sessionLengthSecs = sessionLength * 60;
   }
 
   function startClock() {
+    clockTime.className = "";
     isSession ? startSession() : startBreak();
   }
 
@@ -68,10 +67,10 @@ window.onload = function() {
     timerId = setInterval(function() {
       sessionLengthSecs--;
       clockTime.innerText = getClockTime(sessionLengthSecs);;
-      if(sessionLengthSecs === 0) {
+      if (sessionLengthSecs === 0) {
         clearInterval(timerId);
         isSession = false;
-        sessionLengthSecs = sessionLength*60;
+        sessionLengthSecs = sessionLength * 60;
         sound.play();
         startBreak();
       }
@@ -86,10 +85,10 @@ window.onload = function() {
     timerId = setInterval(function() {
       breakLengthSecs--;
       clockTime.innerText = getClockTime(breakLengthSecs);
-      if(breakLengthSecs === 0) {
+      if (breakLengthSecs === 0) {
         clearInterval(timerId);
         isSession = true;
-        breakLengthSecs = breakLength*60;
+        breakLengthSecs = breakLength * 60;
         sound.play();
         startSession();
       }
@@ -99,11 +98,12 @@ window.onload = function() {
   function resetClock() {
     clearInterval(timerId);
 
+    clockTime.className = "hide"
     clockTime.innerText = sessionLength;
     clockHeading.innerText = "Session"
 
-    breakLengthSecs = breakLength*60;
-    sessionLengthSecs = sessionLength*60;
+    breakLengthSecs = breakLength * 60;
+    sessionLengthSecs = sessionLength * 60;
 
     isClockRunning = false;
     isSession = true;
@@ -113,36 +113,36 @@ window.onload = function() {
 
   function getClockTime(seconds) {
     var hours, mins;
-    if(seconds < 60) {
+    if (seconds < 60) {
       return seconds;
-    } else if(seconds < 3600) {
-      mins = Math.round(seconds/60);
-      seconds = seconds%60;
-      seconds = Math.round((seconds)*100)/100;
-      if(seconds < 10) {
+    } else if (seconds < 3600) {
+      mins = Math.round(seconds / 60);
+      seconds = seconds % 60;
+      seconds = Math.round((seconds) * 100) / 100;
+      if (seconds < 10) {
         seconds = "0" + seconds;
       }
       return mins + ":" + seconds;
     } else {
-      hours = Math.round(seconds/3600);
-      seconds = seconds%3600;
-      mins = Math.round(seconds/60);
-      if(mins < 10) {
+      hours = Math.round(seconds / 3600);
+      seconds = seconds % 3600;
+      mins = Math.round(seconds / 60);
+      if (mins < 10) {
         mins = "0" + mins;
       }
-      seconds = seconds%60;
-      seconds = Math.round((seconds)*100)/100;
-      if(seconds < 10) {
+      seconds = seconds % 60;
+      seconds = Math.round((seconds) * 100) / 100;
+      if (seconds < 10) {
         seconds = "0" + seconds;
       }
-      return hours + ":" + mins + ":" + seconds;  
+      return hours + ":" + mins + ":" + seconds;
     }
   }
 
   resetButton.addEventListener("click", resetClock);
 
-  playPauseButton.addEventListener("click", function() {
+  clock.addEventListener("click", function() {
     isClockRunning = !isClockRunning;
-    isClockRunning? startClock() : pauseClock();
+    isClockRunning ? startClock() : pauseClock();
   })
 };
