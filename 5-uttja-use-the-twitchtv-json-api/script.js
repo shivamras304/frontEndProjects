4636c6c5e3ba304ebb8d5762b2912afcad5e9934
Shivam Rastogi <shivam.ras304@gmail.com>
2017-10-24 09:58:06 +0530

    uttja: Added responsiveness to the app

--- 5-uttja-use-the-twitchtv-json-api/script.js
+++ 5-uttja-use-the-twitchtv-json-api/script.js
@@ -6,32 +6,32 @@ const channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbec
 function loadChannelInfo() {
 
   function getUrl(type, channelName) {
-      const baseUrl = "https://wind-bow.glitch.me/twitch-api/";
-      return baseUrl + type + "/" + channelName + "?callback=?";
+    const baseUrl = "https://wind-bow.glitch.me/twitch-api/";
+    return baseUrl + type + "/" + channelName + "?callback=?";
   }
-    for(const channel of channels) {
-      let gameName, gameStatus;
-      $.getJSON(
-        getUrl("streams", channel),
-        function(data) {
-          if(data.stream === null) {
-            gameName = "Offline";
-            gameStatus = "offline";
-          } else if (data.stream === undefined) {
-            gameName = "Account Closed";
-            gameStatus = "offline";
-          } else {
-            gameName = data.stream.game;
-            gameStatus = "online";
-          }
-          $.getJSON(
-            getUrl("channels", channel),
-            function(data) {
-              const gameLogo = data.logo || "https://goo.gl/a9VX81",
-                gameName = data.display_name || channel,
-                gameDesc = (gameStatus === "online" ? gameName +  ': ' + data.status : "<em>Offline</em>");
+  for (const channel of channels) {
+    let gameName, gameStatus;
+    $.getJSON(
+      getUrl("streams", channel),
+      function(data) {
+        if (data.stream === null) {
+          gameName = "Offline";
+          gameStatus = "offline";
+        } else if (data.stream === undefined) {
+          gameName = "Account Closed";
+          gameStatus = "offline";
+        } else {
+          gameName = data.stream.game;
+          gameStatus = "online";
+        }
+        $.getJSON(
+          getUrl("channels", channel),
+          function(data) {
+            const gameLogo = data.logo || "https://goo.gl/a9VX81",
+              gameName = data.display_name || channel,
+              gameDesc = (gameStatus === "online" ? gameName + ': ' + data.status : "<em>Offline</em>");
 
-              const html = `
+            const html = `
               <li  class="clearfix ${gameStatus}">
                 <img src="${gameLogo}">
                 <div>
@@ -41,11 +41,11 @@ function loadChannelInfo() {
               </li>
               `;
 
-              $("#channelList").append(html);
-            }
-          )
+            $("#channelList").append(html);
+          }
+        )
       })
-    }
+  }
 }
 
 $(document).ready(function() {
@@ -56,18 +56,18 @@ $(document).ready(function() {
     var that = this;
     $("main").slideUp(600, function() {
       var status = $(that).attr('id');
-      switch(status) {
-        case "all" :
+      switch (status) {
+        case "all":
           $(".online").removeClass("hidden");
           $(".offline").removeClass("hidden");
 
           break;
-        case "online" :
+        case "online":
           // $("main ul li").toggleClass("hidden")
           $(".online").removeClass("hidden");
           $(".offline").addClass("hidden");
           break;
-        case "offline" :
+        case "offline":
           $(".offline").removeClass("hidden");
           $(".online").addClass("hidden");
           break;
