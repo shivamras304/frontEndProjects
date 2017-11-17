2ef1403512ca679fc86efd42a5c468b648944fb6
Shivam Rastogi <shivam.ras304@gmail.com>
2017-11-17 13:20:02 +0530

    btttg: Added AIPlayer, AIAction, Game class

---
+++ 8-btttg-build-a-tic-tac-toe-game/scripts/ai.js
@@ -0,0 +1,268 @@
+/*
+ * Constructs an action that the ai player could make
+ * @param pos [Number]: the cell position the ai would make its action in
+ * made that action
+ */
+var AIAction = function(pos) {
+
+  // public : the position on the board that the action would put the board value on
+  this.movePosition = pos;
+
+  //public : the minimax value of the state that the action leads to when applied
+  this.minimaxVal = 0;
+
+  /*
+   * public : applies the action to a state to get the next state
+   * @param state [State]: the state to apply the action to
+   * @return [State]: the next state
+   */
+  this.applyTo = function(state) {
+    var next = new GameState(state);
+
+    //put the corresponding value on the board
+    if(state.turn === state.player1.getName())
+      next.board[this.movePosition] = GameState.BOARD_PLAYER1;
+    else
+      next.board[this.movePosition] = GameState.BOARD_PLAYER2;
+
+    if(state.turn === state.player1.getName())
+      next.oMovesCount++;
+
+    next.advanceTurn();
+
+    return next;
+  }
+};
+
+/*
+ * public static method that defines a rule for sorting AIAction in ascending manner
+ * @param firstAction [AIAction] : the first action in a pairwise sort
+ * @param secondAction [AIAction]: the second action in a pairwise sort
+ * @return [Number]: -1, 1, or 0
+ */
+AIAction.ASCENDING = function(firstAction, secondAction) {
+  if(firstAction.minimaxVal < secondAction.minimaxVal)
+    return -1; //indicates that firstAction goes before secondAction
+  else if(firstAction.minimaxVal > secondAction.minimaxVal)
+    return 1; //indicates that secondAction goes before firstAction
+  else
+    return 0; //indicates a tie
+}
+
+/*
+ * public static method that defines a rule for sorting AIAction in descending manner
+ * @param firstAction [AIAction] : the first action in a pairwise sort
+ * @param secondAction [AIAction]: the second action in a pairwise sort
+ * @return [Number]: -1, 1, or 0
+ */
+AIAction.DESCENDING = function(firstAction, secondAction) {
+  if(firstAction.minimaxVal > secondAction.minimaxVal)
+    return -1; //indicates that firstAction goes before secondAction
+  else if(firstAction.minimaxVal < secondAction.minimaxVal)
+    return 1; //indicates that secondAction goes before firstAction
+  else
+    return 0; //indicates a tie
+}
+
+/*
+ * Constructs an AI player with a specific level of intelligence
+ * @param symbol [String]: symbol of the AI
+ * @param name [String]: name of the AI, usually "Computer"
+ * @param level [String]: the desired level of intelligence
+ */
+var AIPlayer = function(symbol, name, level) {
+
+  GamePlayer.call(symbol, name);
+
+  //private [Number]: level of intelligence the player has
+  var levelOfIntelligence = level;
+
+  //private [Game]: the game the player is playing
+  var game = {};
+
+  /*
+   * private recursive function that computes the minimax value of a game state
+   * @param state [GameState] : the state to calculate its minimax value
+   * @returns [Number]: the minimax value of the state
+   */
+  function minimaxValue(state) {
+    if(state.isTerminal()) {
+      //a terminal game state is the base case
+      return Game.score(state);
+    }
+    else {
+      var stateScore; // this stores the minimax value we'll compute
+
+      if(state.turn === state.player2.getName())
+        // Human wants to maximize --> initialize to a value smaller than any possible score
+        stateScore = -1000;
+      else
+        // AI wants to minimize --> initialize to a value larger than any possible score
+        stateScore = 1000;
+
+      var availablePositions = state.emptyCells();
+
+      //enumerate next available states using the info form available positions
+      var availableNextStates = availablePositions.map(function(pos) {
+        var action = new AIAction(pos);
+
+        var nextState = action.applyTo(state);
+
+        return nextState;
+      });
+
+      /* calculate the minimax value for all available next states
+       * and evaluate the current state's value */
+      availableNextStates.forEach(function(nextState) {
+        var nextScore = minimaxValue(nextState);
+        if(state.turn === state.player2.getName()) {
+          // Human wants to maximize --> update stateScore iff nextScore is larger
+          if(nextScore > stateScore)
+            stateScore = nextScore;
+        }
+        else {
+          // AI wants to minimize --> update stateScore iff nextScore is smaller
+          if(nextScore < stateScore)
+            stateScore = nextScore;
+        }
+      });
+
+      return stateScore;
+    }
+  }
+
+  /*
+   * private function: make the ai player take a blind move
+   * that is: choose the cell to place its symbol randomly
+   * @param turn [String]: the name of the player who has the turn to play
+   */
+  function takeABlindMove(turn) {
+    var available = game.currentState.emptyCells();
+    var randomCell = available[Math.floor(Math.random() * available.length)];
+    var action = new AIAction(randomCell);
+
+    var next = action.applyTo(game.currentState);
+
+    //TODO ui.insertAt(randomCell, turn);
+
+    game.advanceTo(next);
+  }
+
+  /*
+   * private function: make the ai player take a novice move
+   * that is: mix between choosing the optimal and suboptimal minimax decisions
+   * @param turn [String]: the name of the player who has the turn to play
+   */
+  function takeANoviceMove(turn) {
+    var available = game.currentState.emptyCells();
+
+    //enumerate and calculate the score for each available actions to the ai player
+    var availableActions = available.map(function(pos) {
+      var action =  new AIAction(pos); //create the action object
+      var nextState = action.applyTo(game.currentState); //get next state by applying the action
+
+      action.minimaxVal = minimaxValue(nextState); //calculate and set the action's minimax value
+
+      return action;
+    });
+
+    //sort the enumerated actions list by score
+    if(turn === game.currentState.player2.getName())
+      //Human maximizes --> sort the actions in a descending manner to have the action with maximum minimax at first
+      availableActions.sort(AIAction.DESCENDING);
+    else
+    //AI minimizes --> sort the actions in an ascending manner to have the action with minimum minimax at first
+      availableActions.sort(AIAction.ASCENDING);
+
+    /*
+     * take the optimal action 40% of the time, and take the 1st suboptimal action 60% of the time
+     */
+    var chosenAction;
+    if(Math.random()*100 <= 40) {
+      chosenAction = availableActions[0];
+    }
+    else {
+      if(availableActions.length >= 2) {
+        //if there is two or more available actions, choose the 1st suboptimal
+        chosenAction = availableActions[1];
+      }
+      else {
+        //choose the only available actions
+        chosenAction = availableActions[0];
+      }
+    }
+    var next = chosenAction.applyTo(game.currentState);
+
+    //TODO ui.insertAt(chosenAction.movePosition, turn);
+
+    game.advanceTo(next);
+  }
+
+  /*
+   * private function: make the ai player take a blind move
+   * that is: choose the optimal minimax decision
+   * @param turn [String]: the name of the player who has the turn to play
+   */
+  function takeAMasterMove(turn) {
+    var available = game.currentState.emptyCells();
+
+    //enumerate and calculate the score for each avaialable actions to the ai player
+    var availableActions = available.map(function(pos) {
+      var action =  new AIAction(pos); //create the action object
+      var next = action.applyTo(game.currentState); //get next state by applying the action
+
+      action.minimaxVal = minimaxValue(next); //calculate and set the action's minmax value
+
+      return action;
+    });
+
+    //sort the enumerated actions list by score
+    if(turn === game.currentState.player2.getName())
+      //Human maximizes --> sort the actions in a descending manner to have the action with maximum minimax at first
+      availableActions.sort(AIAction.DESCENDING);
+    else
+    //AI minimizes --> sort the actions in an ascending manner to have the action with minimum minimax at first
+      availableActions.sort(AIAction.ASCENDING);
+
+    //take the first action as it's the optimal
+    var chosenAction = availableActions[0];
+    var next = chosenAction.applyTo(game.currentState);
+
+    //TODO ui.insertAt(chosenAction.movePosition, turn);
+
+    game.advanceTo(next);
+  }
+
+  /*
+   * public method to specify the game the ai player will play
+   * basically a setter
+   * @param _game [Game] : the game the ai will play
+   */
+  this.plays = function(_game){
+    game = _game;
+  };
+
+  /*
+   * public function: notify the ai player that it's its turn
+   * @param turn [String]: the name of the player who has the turn to play
+   */
+  this.notify = function(turn) {
+    switch(levelOfIntelligence) {
+      //invoke the desired behavior based on the level chosen
+      case AIPlayer.LEVEL_BLIND: takeABlindMove(turn); break;
+      case AIPlayer.LEVEL_NOVICE: takeANoviceMove(turn); break;
+      case AIPlayer.LEVEL_MASTER: takeAMasterMove(turn); break;
+    }
+  };
+};
+
+//possible intelligence levels of the AI
+AIPlayer.LEVEL_BLIND = 0;
+AIPlayer.LEVEL_NOVICE = 1;
+AIPlayer.LEVEL_MASTER = 2;
+
+/*
+ * AIPlayer inherits from GamePlayer
+ */
+AIPlayer.prototype = Object.create(GamePlayer)
+AIPlayer.protoype.constructor = AIPlayer;
--- 8-btttg-build-a-tic-tac-toe-game/scripts/game.js
+++ 8-btttg-build-a-tic-tac-toe-game/scripts/game.js
@@ -6,34 +6,39 @@
 var GameState = function(oldState) {
 
   /*
-   * public [GamePlayer] : Player 1 (Usually AI)
+   * public [GamePlayer]: Player 1 (Usually AI)
    */
   this.player1 = "";
 
   /*
-   * public [GamePlayer] : Name of player 2 (Usually Human)
+   * public [GamePlayer]: Player 2 (Usually Human)
    */
   this.player2 = "";
 
   /*
-   * public [String] : name of the player who has the turn to make a move
+   * public [String]: name of the player who has the turn to make a move
    */
   this.turn = "";
 
   /*
-   * public [number] : number of Moves of the AI player
+   * public [Number]: number of Moves of the AI player
    */
   this.oMovesCount = 0;
 
   /*
-   * public [number] : the result of the game in this State
+   * public [Number]: the result of the game in this State
    */
   this.result = GameState.INVALID;
 
   /*
-   * public [Array] : the board configuration in this state
+   * public [Array]: the board configuration in this state
+   * the board will have 3 possible values:
+   * IMPORTANT
+   * 0 : Empty cell
+   * 1 : Player 1's move
+   * 2 : Player 2's move
    */
-   this.board = [];
+  this.board = [];
 
   if(oldState !== undefined) {
     // If the state is constructed using a copy of another state
@@ -52,17 +57,18 @@ var GameState = function(oldState) {
    * public : switches the turn between the two players for the state
    */
   this.advanceTurn = function() {
-    this.turn = this.turn === player1 ? this.player2 : this.player1;
+    this.turn = this.turn === player1.getName() ?
+      this.player2.getName() : this.player1.getName();
   }
 
   /*
    * public function that enumerates the empty cells in state
    * @return [Array]: indices of all empty cells
    */
-  this.emptyCells = function() {
+  this.getEmptyCells = function() {
     var indices = [];
     for(var itr = 0; itr < 9 ; itr++) {
-      if(this.board[itr] === "E") {
+      if(this.board[itr] === GameState.BOARD_EMPTYCELL) {
         indices.push(itr);
       }
     }
@@ -75,66 +81,74 @@ var GameState = function(oldState) {
    * @returns [Boolean]: true if it's terminal, false otherwise
    */
   this.isTerminal = function() {
-      var B = this.board;
-      var that = this;
+    var B = this.board;
+    var that = this;
 
-      function updateResultForWinner(symbol) {
-        if(symbol === that.player1.getSymbol()) {
-          that.result = GameState.PLAYER1_WINS;
-        } else {
-          that.result = GameState.PLAYER2_WINS;
-        }
+    function updateResultForWinner(boardSymbol) {
+      if(boardSymbol === GameState.BOARD_PLAYER1) {
+        that.result = GameState.PLAYER1_WINS;
+      } else {
+        that.result = GameState.PLAYER2_WINS;
       }
+    }
 
-      //check rows
-      for(var i = 0; i <= 6; i = i + 3) {
-          if(B[i] !== "E" && B[i] === B[i + 1] && B[i + 1] == B[i + 2]) {
-              updateResultForWinner(B[i]) //update the state result
-              return true;
-          }
+    //check rows
+    for(var i = 0; i <= 6; i = i + 3) {
+      if(B[i] !== GameState.BOARD_EMPTYCELL && B[i] === B[i + 1] && B[i + 1] == B[i + 2]) {
+        updateResultForWinner(B[i]) //update the state result
+        return true;
       }
+    }
 
-      //check columns
-      for(var i = 0; i <= 2 ; i++) {
-          if(B[i] !== "E" && B[i] === B[i + 3] && B[i + 3] === B[i + 6]) {
-              updateResultForWinner(B[i]) //update the state result
-              return true;
-          }
+    //check columns
+    for(var i = 0; i <= 2 ; i++) {
+      if(B[i] !== GameState.BOARD_EMPTYCELL && B[i] === B[i + 3] && B[i + 3] === B[i + 6]) {
+        updateResultForWinner(B[i]) //update the state result
+        return true;
       }
+    }
 
-      //check diagonals
-      for(var i = 0, j = 4; i <= 2 ; i = i + 2, j = j - 2) {
-          if(B[i] !== "E" && B[i] == B[i + j] && B[i + j] === B[i + 2*j]) {
-              updateResultForWinner(B[i]) //update the state result
-              return true;
-          }
+    //check diagonals
+    for(var i = 0, j = 4; i <= 2 ; i = i + 2, j = j - 2) {
+      if(B[i] !== GameState.BOARD_EMPTYCELL && B[i] == B[i + j] && B[i + j] === B[i + 2*j]) {
+        updateResultForWinner(B[i]) //update the state result
+        return true;
       }
+    }
 
-      var available = this.emptyCells();
-      if(available.length == 0) {
-          //the game is draw
-          this.result = GameState.GAME_DRAW; //update the state result
-          return true;
-      }
-      else {
-          return false;
-      }
+    var available = this.emptyCells();
+    if(available.length === 0) {
+      //the game is draw
+      this.result = GameState.GAME_DRAW; //update the state result
+      return true;
+    }
+    else {
+      return false;
+    }
   };
 }
+
+//possible results of a game state
 GameState.INVALID = -1;
 GameState.GAME_RUNNING = 0;
 GameState.PLAYER1_WINS = 1;
 GameState.PLAYER2_WINS = 2;
 GameState.GAME_DRAW = 3;
 
+//possible values in the board Array
+GameState.BOARD_EMPTYCELL = 0;
+GameState.BOARD_PLAYER1 = 1;
+GameState.BOARD_PLAYER2 = 2;
 /*
  * Represents basic requirements of a game player
- * @param symbol: Either X or O as chosen by the player(s)
- * @param name: Name of the player
+ * @param symbol [String]: Either X or O as chosen by the player(s)
+ * @param name [String]: Name of the player
  * TODO two players cannot have same name!
  */
 var GamePlayer = function(_symbol, _name) {
+  // private
   var symbol = _symbol;
+  // private
   var name = _name;
 
   this.getSymbol() {
@@ -143,5 +157,103 @@ var GamePlayer = function(_symbol, _name) {
 
   this.getName() {
     return name;
+  }
+}
+
+/*
+ * Constructs a game object to be played
+ * @param autoPlayer [AIPlayer] : the AI player to be play the game with
+ * this is what glues all the components together
+ */
+var Game = function(aiPlayer, humanPlayer) {
+
+    //public [AIPlayer]: initialize the ai player for this game
+    this.aiPlayer = aiPlayer;
+
+    //public [HumanPlayer]: initialize the human player for this game
+    this.humanPlayer = humanPlayer;
+
+    // public : initialize the game current state to empty board configuration
+    this.currentState = new State();
+
+    this.currentState.board =
+      [GameState.BOARD_EMPTYCELL, GameState.BOARD_EMPTYCELL, GameState.BOARD_EMPTYCELL,
+       GameState.BOARD_EMPTYCELL, GameState.BOARD_EMPTYCELL, GameState.BOARD_EMPTYCELL,
+       GameState.BOARD_EMPTYCELL, GameState.BOARD_EMPTYCELL, GameState.BOARD_EMPTYCELL];
+
+    this.currentState.turn = this.humanPlayer.getName(); //Human plays first
+    this.currentState.player1 = this.aiPlayer;
+    this.currentState.player2 = this.humanPlayer;
+    /*
+     * initialize game status to beginning
+     */
+    this.status = Game.STATUS_BEGINNING;
+
+    /*
+     * public function that advances the game to a new state
+     * @param _state [State]: the new state to advance the game to
+     */
+    this.advanceTo = function(_state) {
+      this.currentState = _state;
+      if(_state.isTerminal()) {
+        this.status = Game.STATUS_ENDED;
+
+        if(_state.result === GameState.PLAYER2_WINS)
+          //Human won
+          //TODO ui.switchViewTo("won");
+        else if(_state.result === GameState.PLAYER1_WINS)
+          //Human lost
+          //TODO ui.switchViewTo("lost");
+        else
+          //it's a draw
+          //TODO ui.switchViewTo("draw");
+      }
+      else {
+        //the game is still running
+        if(this.currentState.turn === this.currentState.player2.getName()) {
+          //TODO ui.switchViewTo("human");
+        }
+        else {
+          //TODO ui.switchViewTo("robot");
+
+          //notify the AI player its turn has come up
+          this.aiPlayer.notify(this.aiPlayer.getName());
+        }
+      }
+    };
+
+    /*
+     * starts the game
+     */
+    this.start = function() {
+      if(this.status = Game.STATUS_BEGINNING) {
+        //invoke advanceTo with the initial state
+        this.advanceTo(this.currentState);
+        this.status = Game.STATUS_RUNNING;
+      }
+    }
+};
+//possible values for status
+Game.STATUS_BEGINNING = 0;
+Game.STATUS_RUNNING = 1;
+Game.STATUS_ENDED = 2;
+
+/*
+ * public static function that calculates the score of the human player in a given terminal state
+ * @param _state [State]: the state in which the score is calculated
+ * @return [Number]: the score calculated for the human player
+ */
+Game.score = function(_state) {
+  if(_state.result === GameState.PLAYER2_WINS){
+    // the human player won
+    return 10 - _state.oMovesCount;
+  }
+  else if(_state.result === GameState.PLAYER1_WINS) {
+    //the human player lost
+    return - 10 + _state.oMovesCount;
+  }
+  else {
+    //it's a draw
+    return 0;
   }
 }
